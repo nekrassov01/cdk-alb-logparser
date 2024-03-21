b8d150f4f33809566b8c2f2e6885c5b3159aaeff
nekrassov01 <unclek62@gmail.com>
2024-03-21 13:16:30 +0900

    chore: fix package name

--- test/aws-alb-logparser.test.ts
+++ test/aws-alb-logparser.test.ts
@@ -1,9 +1,9 @@
 // import * as cdk from 'aws-cdk-lib';
 // import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsDrSample from '../lib/cdk-dr-sample-stack';
+// import * as AwsDrSample from '../lib/cdk-alb-logparser-stack';
 
 // example test. To run these tests, uncomment this file along with the
-// example resource in lib/cdk-dr-sample-stack.ts
+// example resource in lib/cdk-alb-logparser-stack.ts
 test("SQS Queue Created", () => {
   //   const app = new cdk.App();
   //     // WHEN
